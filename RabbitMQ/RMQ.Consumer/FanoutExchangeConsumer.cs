4469bc34033a948ed336f84a6414237cecada909
ddhuissier <ddhuissier@gmail.com>
2021-09-19 18:32:18 +0200

    Implementation fanout exchange (Publish/Subscribe)

---
+++ RabbitMQ/RMQ.Consumer/FanoutExchangeConsumer.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RMQ.Consumer
+{
+    public static class FanoutExchangeConsumer
+    {
+        public static void Consume(IModel channel)
+        {
+            channel.ExchangeDeclare("fanout-exchange", ExchangeType.Fanout);
+            channel.QueueDeclare("fanout-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+
+            channel.QueueBind("fanout-queue", "fanout-exchange", string.Empty);
+            channel.BasicQos(0, 10, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) => {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("fanout-queue", true, consumer);
+            Console.WriteLine("Consumer started");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ/RMQ.Producer/FanoutExchangePublisher.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace RMQ.Producer
+{
+    static class FanoutExchangePublisher
+    {
+        public static void Publish(IModel channel)
+        {
+            var ttl = new Dictionary<string, object>
+            {
+                {"x-message-ttl", 30000 }
+            };
+            channel.ExchangeDeclare("fanout-exchange", ExchangeType.Fanout, arguments: ttl);
+            var count = 0;
+
+            while (true)
+            {
+                var message = new { Name = "Producer", Message = $"Send msg [{count}]" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                var properties = channel.CreateBasicProperties();
+                properties.Headers = new Dictionary<string, object> { { "account", "update" } };
+
+                channel.BasicPublish("fanout-exchange", "account.new", properties, body);
+                count++;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
